20089de51dc53f74547554f9efd4cd804dfd0eaa
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-11-29 21:30:36 +0530

    Document the app.module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+
+// We use require here, because the new import system doesn't work with cookie-session
 const cookieSession = require('cookie-session');
 
 @Module({
