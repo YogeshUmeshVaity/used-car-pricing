2fdba1e48e02b826147f95bf14cf2a1cb5a0a3e1
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-06 04:17:54 +0530

    Document the user.entity to explain the concept of circular dependency.

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -24,6 +24,21 @@ export class User {
   @Column({ default: true })
   admin: boolean;
 
+  /**
+   * Circular dependency explained:
+   * We import Report entity in the User entity file and import User entity in the Report entity
+   * file. This is called circular dependency. When that happens only of them exist at one
+   * particular time and the other is undefined while the files are being loaded. You can test this
+   * by writing console.log(Report) in the user.entity file and console.log(User) in the
+   * report.entity file. Only of them will be defined and the other will print undefined.
+   *
+   * Due to this circular dependency, we can't directly make reference to the Report entity inside
+   * the User entity file and vice versa. To solve this problem, we wrap the Report entity in a
+   * function () => Report. Once both these files get executed, only after that at some point in
+   * time in the future when our code is actually running and we need to actually figure out what
+   * is meant by this relationship does this function gets executed. And at that point in time the
+   * User and Report in the other file will actually be defined.
+   */
   @OneToMany(() => Report, (report) => report.user)
   reports: Report[];
 
