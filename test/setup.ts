3c95d4c16c71f3c23244798c326be7229e0b4546
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-06 04:16:10 +0530

    Document the global beforeEach() and afterEach() in test/setup.ts file.

--- test/setup.ts
+++ test/setup.ts
@@ -2,12 +2,20 @@ import { rm } from 'fs/promises';
 import { join } from 'path';
 import { getConnection } from 'typeorm';
 
+/**
+ * Database file is deleted before each test in each file is starts.
+ */
 global.beforeEach(async () => {
   try {
     await rm(join(__dirname, '..', 'test.sqlite'));
   } catch (err) {}
 });
 
+/**
+ * Typeorm doesn't know that we deleted the file. So, we close the connection so that Typeorm
+ * doesn't hold the connection to the deleted database. This will result in Typeorm creating a new
+ * connection for the next test. This executes every test in each file, not just every file.
+ */
 global.afterEach(async () => {
   const conn = getConnection();
   await conn.close();
